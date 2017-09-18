6a25b7b669fb8909200079441c6ee1a361615c94
christ <Christ.li@sinodynamic.com>
2017-09-18 14:01:09 +0800

    CODE

---
+++ dubbo-demo-consumer/src/main/java/com/alibaba/dubbo/demo/consumer/Consumer.java
@@ -0,0 +1,28 @@
+package com.alibaba.dubbo.demo.consumer;
+
+import com.alibaba.dubbo.demo.DemoService;
+
+import java.util.List;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by ken.lj on 2017/7/31.
+ */
+public class Consumer {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"META-INF/spring/dubbo-demo-consumer.xml"});
+        context.start();
+
+        DemoService demoService = (DemoService) context.getBean("demoService"); // 获取远程服务代理
+        String hello = demoService.sayHello("world"); // 执行远程方法
+
+        System.out.println("consumer##########:"+hello); // 显示调用结果
+        
+        List<Object>aa=demoService.getList();
+        for (Object object : aa) {
+        	System.out.println("$$$$$$$$$:"+object);
+		}
+    }
+}
